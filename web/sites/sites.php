734c52662fc996ee22112b6b8ec0065bb17e1c3c
Christophe Espiau <christophe.espiau@gmail.com>
2018-07-18 19:22:54 +0200

    Création du fichier sites.php.

---
+++ web/sites/sites.php
@@ -0,0 +1,50 @@
+<?php
+
+// @codingStandardsIgnoreFile
+
+/**
+ * @file
+ * Configuration file for multi-site support and directory aliasing feature.
+ */
+
+$sites = array(
+  // Local sites
+  'www.arpont.localhost' => 'arpont',
+  'www.bois.localhost' => 'bois',
+  'www.femma.localhost' => 'femma',
+  'www.fours.localhost' => 'fours',
+  'www.lac.localhost' => 'lac',
+  'www.leisse.localhost' => 'leisse',
+  'www.martin.localhost' => 'martin',
+  'www.orgere.localhost' => 'orgere',
+  'www.palet.localhost' => 'palet',
+  'www.plaisance.localhost' => 'plaisance',
+  'www.prariond.localhost' => 'prariond',
+  'www.rosuel.localhost' => 'rosuel',
+  'www.turia.localhost' => 'turia',
+  'www.valette.localhost' => 'valette',
+  'www.vallonbrun.localhost' => 'vallonbrun',
+
+  // Korora Dev sites
+  'refuge-arpont.korora.fr' => 'arpont',
+  'refuge-bois.korora.fr' => 'bois',
+  'refuge-femma.korora.fr' => 'femma',
+  'refuge-fours.korora.fr' => 'fours',
+  'refuge-lac.korora.fr' => 'lac',
+  'refuge-leisse.korora.fr' => 'leisse',
+  'refuge-martin.korora.fr' => 'martin',
+  'refuge-orgere.korora.fr' => 'orgere',
+  'refuge-palet.korora.fr' => 'palet',
+  'refuge-plaisance.korora.fr' => 'plaisance',
+  'refuge-prariond.korora.fr' => 'prariond',
+  'refuge-rosuel.korora.fr' => 'rosuel',
+  'refuge-turia.korora.fr' => 'turia',
+  'refuge-valette.korora.fr' => 'valette',
+  'refuge-vallonbrun.korora.fr' => 'vallonbrun',
+
+  // Staging sites
+
+
+  // Production sites
+
+);
