d8a7dabd84d2a7e3b04f8a69919083f045550d05
Christophe Espiau <christophe.espiau@gmail.com>
2018-09-22 15:49:31 +0200

    Création du type de contenu Galerie photo et de sa vue.

---
+++ web/themes/custom/refuges/js/slick.photo-galleries.js
@@ -0,0 +1,33 @@
+/**
+ * Created by ubuntu on 21/09/18.
+ */
+
+/**
+ * Created by ubuntu on 30/07/18.
+ */
+
+(function ($) {
+  Drupal.behaviors.photoGalleriesSlider = {
+    attach: function (context, settings) {
+
+      // Photo gallery slider : photo display
+      $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+      });
+
+      // Photo gallery slider : navigator
+      $('.slider-nav').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        arrows: true,
+        centerMode: true,
+        focusOnSelect: true,
+      });
+    }
+  };
+})(jQuery);
