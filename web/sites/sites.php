1b55814dd98a173b3ed0591546e3000ccf7ae978
Christophe Espiau <christophe.espiau@gmail.com>
2018-09-28 17:34:21 +0200

    Modification du fichier sites.php : ajout des noms de domaine de recette et de prod.

--- web/sites/sites.php
+++ web/sites/sites.php
@@ -43,8 +43,36 @@ $sites = array(
   'refuge-vallonbrun.korora.fr' => 'vallonbrun',
 
   // Staging sites
-
+  'pnv-refuge-arpont.brgm-rec.fr' => 'arpont',
+  'pnv-refuge-dubois.brgm-rec.fr' => 'bois',
+  'pnv-refuge-femma.brgm-rec.fr' => 'femma',
+  'pnv-refuge-fonddesfours.brgm-rec.fr' => 'fours',
+  'pnv-refuge-plandulac.brgm-rec.fr' => 'lac',
+  'pnv-refuge-leisse.brgm-rec.fr' => 'leisse',
+  'pnv-refuge-lamartin.brgm-rec.fr' => 'martin',
+  'pnv-refuge-orgere.brgm-rec.fr' => 'orgere',
+  'pnv-refuge-coldupalet.brgm-rec.fr' => 'palet',
+  'pnv-refuge-plaisance.brgm-rec.fr' => 'plaisance',
+  'pnv-refuge-prariond.brgm-rec.fr' => 'prariond',
+  'pnv-refuge-rosuel.brgm-rec.fr' => 'rosuel',
+  'pnv-refuge-turia.brgm-rec.fr' => 'turia',
+  'pnv-refuge-valette.brgm-rec.fr' => 'valette',
+  'pnv-refuge-vallonbrun.brgm-rec.fr' => 'vallonbrun',
 
   // Production sites
-
+  'refuge-arpont.vanoise.com' => 'arpont',
+  'refuge-dubois.vanoise.com' => 'bois',
+  'refuge-femma.vanoise.com' => 'femma',
+  'refuge-fonddesfours.vanoise.com' => 'fours',
+  'refuge-plandulac.vanoise.com' => 'lac',
+  'refuge-leisse.vanoise.com' => 'leisse',
+  'refuge-lamartin.vanoise.com' => 'martin',
+  'refuge-orgere.vanoise.com' => 'orgere',
+  'refuge-coldupalet.vanoise.com' => 'palet',
+  'refuge-plaisance.vanoise.com' => 'plaisance',
+  'refuge-prariond.vanoise.com' => 'prariond',
+  'refuge-rosuel.vanoise.com' => 'rosuel',
+  'refuge-turia.vanoise.com' => 'turia',
+  'refuge-valette.vanoise.com' => 'valette',
+  'refuge-vallonbrun.vanoise.com' => 'vallonbrun',
 );
