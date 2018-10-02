9251ab27d64793e6094f4405151176e76f3b8a0c
Christophe Espiau <christophe.espiau@gmail.com>
2018-10-02 17:28:34 +0200

    Intégration du menu Hamburger Bootstrap... suite.

--- web/themes/custom/refuges/js/bootstrap.hamburger-menu.js
+++ web/themes/custom/refuges/js/bootstrap.hamburger-menu.js
@@ -5,29 +5,21 @@
 (function ($) {
   Drupal.behaviors.hamburgerMenu = {
     attach: function (context, settings) {
-      $('#dismiss, .overlay').on('click', function () {
+      $('.hamburger-close, .overlay').on('click', function () {
+        $(this).hide();
+        $('.hamburger-open').show();
         $('#sidebar').removeClass('active');
         $('.hbg-overlay').fadeOut();
       });
 
-      $('#sidebarCollapse').on('click', function () {
-        // C.Espiau - Ajoute un padding top au menu hamburger si user connecté
-        // pour que la barre d'admin ne cache pas le haut du menu hamburger.
-        //if ($('#toolbar-administration').length) {
-        //  var paddingTop = $('#toolbar-administration .toolbar-bar').height() + $('.toolbar-tray').height();
-        //  $('#sidebar').css('padding-top', paddingTop + 'px');
-        //  $('#dismiss').css('top', paddingTop + 'px');
-        //}
+      $('.hamburger-open').on('click', function () {
+        $(this).hide();
+        $('.hamburger-close').show();
 
-        //console.log($('div.hamburger').parent().height());
-        //$('#sidebar').css('top', $('div.hamburger').parent().height() + 'px');
+        // Permet de descendre le menu hamburger sous le header.
         var position = $('.dialog-off-canvas-main-canvas > section:nth-child(1) > div:nth-child(2)').position();
         var bodyMarginTop = $('body').css('margin-top');
         var verticalOffset = Math.round(parseFloat(position.top) + parseFloat(bodyMarginTop));
-
-        console.log(position.top);
-        console.log(bodyMarginTop);
-        console.log(verticalOffset);
         $('#sidebar').css('top', verticalOffset + 'px');
 
         $('#sidebar').addClass('active');
