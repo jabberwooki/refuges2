5aa47b134153586c5148376742fb2b778d726383
Christophe Espiau <christophe.espiau@gmail.com>
2018-10-02 21:49:02 +0200

    Création du javascript pour faire apparaitre le formulaire de recherche, mais inactivé pour l'instant.

---
+++ web/themes/custom/refuges/js/searchbar.js
@@ -0,0 +1,17 @@
+/**
+ * Created by ubuntu on 02/10/18.
+ */
+
+
+(function ($) {
+  Drupal.behaviors.searchBar = {
+    attach: function (context, settings) {
+      //$('#search-block-form').hide();
+      //
+      //$('span.glyphicon-search').one('click', function(){
+      //  $('#search-block-form').slideToggle();
+      //  event.stopImmediatePropagation();
+      //});
+    }
+  };
+})(jQuery);
