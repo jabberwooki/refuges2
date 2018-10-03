b00b0802d4f3580da6ee89c91e961bd5aec653d5
Christophe Espiau <christophe.espiau@gmail.com>
2018-10-03 08:14:47 +0200

    Correction du javascript d'ouverture du formulaire de recherche. Ajout du bouton de fermeture (glyphicon-remove).

--- web/themes/custom/refuges/js/searchbar.js
+++ web/themes/custom/refuges/js/searchbar.js
@@ -2,16 +2,21 @@
  * Created by ubuntu on 02/10/18.
  */
 
-
 (function ($) {
   Drupal.behaviors.searchBar = {
     attach: function (context, settings) {
-      //$('#search-block-form').hide();
-      //
-      //$('span.glyphicon-search').one('click', function(){
-      //  $('#search-block-form').slideToggle();
-      //  event.stopImmediatePropagation();
-      //});
+      $('.region-top .search-block-form').hide();
+
+      $('span.glyphicon-search').on('click', function(event){
+        $('.region-top .search-block-form').slideToggle();
+        event.stopImmediatePropagation();
+        $('#search-block-form input[type="search"]').focus();
+      });
+
+      $('#searchformclose').on('click', function(event){
+        $('.region-top .search-block-form').slideToggle();
+        event.stopImmediatePropagation();
+      });
     }
   };
 })(jQuery);
