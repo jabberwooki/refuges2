d51d493066adbd31cabc316b40be4af85ea01248
Christophe Espiau <christophe.espiau@gmail.com>
2018-10-05 20:12:07 +0200

    Ajout du code javascript pour refermer le menu hamburger dès qu'on clique ailleurs sur le document.

--- web/themes/custom/refuges/js/bootstrap.hamburger-menu.js
+++ web/themes/custom/refuges/js/bootstrap.hamburger-menu.js
@@ -12,6 +12,17 @@
       var verticalOffset = Math.round(parseFloat(position.top) + parseFloat(bodyMarginTop));
       $('#sidebar').css('top', verticalOffset + 'px');
 
+      // Referme le menu hamburger si l'on clique n'importe ou dans le document
+      // sauf sur le menu lui-même ou le picto menu.
+      $(document).click(function(e) {
+        if ($(e.target).closest("#sidebar").length === 0 && $(e.target).closest("#sidebarCollapse").length === 0) {
+          $('.hamburger-close').hide();
+          $('.hamburger-open').show();
+          $('#sidebar').removeClass('active');
+          $('.hbg-overlay').fadeOut();
+        }
+      });
+
       $('.hamburger-close, .overlay').on('click', function () {
         $(this).hide();
         $('.hamburger-open').show();
@@ -22,20 +33,11 @@
       $('.hamburger-open').on('click', function () {
         $(this).hide();
         $('.hamburger-close').show();
-
-        // Permet de descendre le menu hamburger sous le header.
-        //var position = $('.dialog-off-canvas-main-canvas > section:nth-child(1) > div:nth-child(2)').position();
-        //var bodyMarginTop = $('body').css('margin-top');
-        //var verticalOffset = Math.round(parseFloat(position.top) + parseFloat(bodyMarginTop));
-        //$('#sidebar').css('top', verticalOffset + 'px');
-
         $('#sidebar').addClass('active');
         $('.hbg-overlay').fadeIn();
         $('.collapse.in').toggleClass('in');
         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
       });
-
-
     }
   };
 })(jQuery);
