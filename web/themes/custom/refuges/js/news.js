ffb3a516f56a02f49ec77ae98c288f8abd2d60e5
Christophe Espiau <christophe.espiau@gmail.com>
2018-11-21 12:34:33 +0100

    Utilistation de la librairie jquery.ellipsis pour gérer le nombre de lignes des résumés.

--- web/themes/custom/refuges/js/news.js
+++ web/themes/custom/refuges/js/news.js
@@ -5,10 +5,35 @@
 (function ($) {
   Drupal.behaviors.news = {
     attach: function (context, settings) {
+      // Actus - Le div Texte doit avoir la même hauteur que le div Image.
       var imageHeight = $('.view-news .field--name-field-media-image').height();
       $('.view-news .text-teaser').each(function() {
         $(this).css('height', imageHeight + 'px');
       });
+
+      // Actus : Contrôle du nombre de lignes de résumé en page d'accueil
+      $('.view-news.view-display-id-frontpage_block .field--name-field-summary').ellipsis(
+        {
+          lines: 7,
+          responsive: true
+        }
+      );
+      // Actus : Contrôle du nombre de lignes de résumé dans la rubrique Actualités
+      // 
+      $('.view-news.view-display-id-list .field--name-field-summary').ellipsis(
+        {
+          lines: 4,
+          responsive: true
+        }
+      );
+
+      // Agenda : Contrôle du nombre de lignes de résumé en page d'accueil
+      // $('.view-events .field--name-field-summary').ellipsis(
+      //   {
+      //     lines: 4,
+      //     responsive: true
+      //   }
+      // );
     }
   };
 })(jQuery);
